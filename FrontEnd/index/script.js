dcb37653521e2354a84f1ccb16853b71b22d02aa
曦暮流年 <2515550574@qq.com>
2023-12-13 15:10:56 +0800

    更新

--- FrontEnd/index/script.js
+++ FrontEnd/index/script.js
@@ -13,47 +13,63 @@ $("#signin").click(function () {
 
 // 登录模块
 var dlBut = function () {
-    axios.post(AjaxURL + "/sel/users", {
-        withCredentials: true,
-        userName: document.getElementById("d_zh").value,
-        password: document.getElementById("d_mm").value
-    })
-        // 执行成功进行跳转
-        .then(function (response) {
-            const data = response.data;
+    // 先使用验证码进行人机验证
+    axios.get(AjaxURL + "/captcha/verify/" + document.getElementById("d_yzm").value)
 
-            // 判断是否返回成功
-            if (typeof (date) == "string" || data == 'passwordError') {
-                showPrompt("登录失败,可能是账号密码错误", "red", 3000);
+        .then(function (responseyzm) {
+            if (responseyzm.data) {
+                axios.post(AjaxURL + "/sel/users", {
+                    withCredentials: true,
+                    userName: document.getElementById("d_zh").value,
+                    password: document.getElementById("d_mm").value
+                })
+                    // 执行成功进行跳转
+                    .then(function (response) {
+                        const data = response.data;
+
+                        // 判断是否返回成功
+                        if (typeof (date) == "string" || data == 'passwordError') {
+                            showPrompt("登录失败,可能是账号密码错误", "red", 3000);
+                        } else {
+                            showPrompt("登录成功", "greenyellow", 3000);
+                            window.location.href = "/lt.html?uuid=" + data;
+                        }
+                    })
+                    // 登录失败进行提示信息
+                    .catch(function (error) {
+                        showPrompt("请求登录失败", "red", 3000);
+                    })
             } else {
-                showPrompt("登录成功", "greenyellow", 3000);
-                window.location.href = "/lt.html?uuid=" + data;
+                showPrompt("验证码错误", "red", 3000);
             }
-        })
-        // 登录失败进行提示信息
-        .catch(function (error) {
-            showPrompt("请求登录失败", "red", 3000);
         })
 }
 
 // 注册模块
 var zcBut = function () {
-    axios.post(AjaxURL + "/ins/user", {
-        userName: document.getElementById("z_zh").value,
-        password: document.getElementById("z_mm").value
-    })
-        // 执行注册成功进行跳转
-        .then(function (response) {
-            if (response.data == 'lengthError') {
-                showPrompt("注册失败，密码和账号要求 6-20 位之间", "red", 3000);
-            } else if (response.data == 'existError') {
-                showPrompt("注册失败，账号已存在", "red", 3000);
+    axios.get(AjaxURL + "/captcha/verify/" + document.getElementById("d_yzm").value)
+        .then(function (responseyzm) {
+            if (responseyzm.data) {
+                axios.post(AjaxURL + "/ins/user", {
+                    userName: document.getElementById("z_zh").value,
+                    password: document.getElementById("z_mm").value
+                })
+                    // 执行注册成功进行跳转
+                    .then(function (response) {
+                        if (response.data == 'lengthError') {
+                            showPrompt("注册失败，密码和账号要求 6-20 位之间", "red", 3000);
+                        } else if (response.data == 'existError') {
+                            showPrompt("注册失败，账号已存在", "red", 3000);
+                        } else {
+                            showPrompt("注册成功", "greenyellow", 3000);
+                        }
+                    })
+                    // 注册失败进行提示信息
+                    .catch(function (error) {
+                        showPrompt("请求注册失败", "red", 3000);
+                    })
             } else {
-                showPrompt("注册成功", "greenyellow", 3000);
+                showPrompt("验证码错误", "red", 3000);
             }
-        })
-        // 注册失败进行提示信息
-        .catch(function (error) {
-            showPrompt("请求注册失败", "red", 3000);
         })
 }
